8e9e7e7e6fd52b0065042a8a458a82d8dfdfa8c0
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-08-20 17:26:52 -0400

    testing db using seed

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,88 +1,95 @@
 #!/usr/bin/env python3
-# seed.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import User, Trip, Expense
 import random
 from datetime import datetime, timedelta
-
-def seed_users(session):
-    names = ['John', 'Evan', 'Emma', 'Ana', 'Heshan', 'Shaan', 'Zoe', 'zia', 'rani', 'nila']
-    phones = ['5454545454', '3453453452', '1234123443', '6565657890']
-    
-    users = []
-    for _ in range(10):
-        user = User(
-            user_name=random.choice(names),
-            Email=f'{random.choice(names)}@gmail.com',
-            Phone=random.choice(phones),
-            created_at=datetime.now()
-        )
-        #session.add(user)
-        users.append(user)
-    #session.commit()
-    return users
-def add_new_user(session, user_name, email, phone):
-    new_user = User(user_name=user_name, Email=email, Phone=phone, created_at=datetime.now())
-    session.add(new_user)
-    session.commit()
-    return new_user
-
-def add_new_trip(session, start_place, destination_place, gas_cost, mileage, user):
-    new_trip = Trip(start_place=start_place, destination_place=destination_place, gas_cost=gas_cost, mileage=mileage, user=user)
-    session.add(new_trip)
-    session.commit()
-    return new_trip
-
-def add_new_expense(session, cost_category, amount, trip):
-    new_expense = Expense(cost_category=cost_category, amount=amount, trip=trip)
-    session.add(new_expense)
-    session.commit()
-
-def seed_trips(session, users):
-    start_places = ["NewYork", "NewJersey", "California", "niagarafalls US"]
-    destination_places = ["Chicago", "Ohio", "Connecticut","Nevada"]
-    
-    trips = []
-    for _ in range(10):
-        user = random.choice(users)
-        trip = add_new_trip(
-            session,
-            start_place=random.choice(start_places),
-            destination_place=random.choice(destination_places),
-            gas_cost=random.randint(10, 555),
-            mileage=random.randint(20, 65),
-            user=user
-        )
-        trips.append(trip)
-    return trips
-
-def seed_expenses(session, trips):
-    categories = ["Food", "Accommodation", "Entrance", "Parking"]
-    
-    for _ in range(10):
-        trip = random.choice(trips)
-        add_new_expense(
-            session,
-            cost_category=random.choice(categories),
-            amount=random.randint(100, 500),
-            trip=trip
-        )
-
+# from geopy.geocoders import Nominatim
+# from geopy.distance import geodesic
 if __name__ == '__main__':
+    #databasse connection
     engine = create_engine('sqlite:///trip_records.db')
+    #Base.metadata.create_all(engine)
+
+    #create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
+#deleting old populated seed to make sure everything works fine
     session.query(User).delete()
     session.query(Trip).delete()
     session.query(Expense).delete()
-    print("Starting seed populates")
+    print("starting seed populates")
     session.commit()
 
-    users = seed_users(session)
-    trips = seed_trips(session, users)
-    seed_expenses(session, trips)
+#insert seed data
+    names = ['John', 'Evan', 'Emma', 'Ana', 'Heshan', 'Shaan', 'Zoe', 'zia', 'rani', 'nila']
+    #email = f'{names}@gmail.com'
+    phones = ['5454545454', '3453453452', '1234123443', '6565657890']
+    emails = ['abc@gmail.com', 'wer@gmail.com','re123@gmail.com', 'frg23@hotmail.com','wdv@gmail.com','frg@gmail.com','tre@ymail.com']
+    #start_date = datetime(2023, 1, 1)
+    #end_date = datetime(2023, 12, 31)
+    #time_difference = end_date - start_date
+    #random_time = random.random()* time_difference.total_seconds()
+            
+    users = []
+    for name, phone, email in zip(names,phones,emails):
+        #zip iterates through names,phones,emails lists simultaneously
+        #random_seconds = random.randint(0,int(time_difference.total_seconds()))
+        #random_time = start_date + timedelta(seconds=random_seconds)
+        user = User(
+            user_name=name,
+            Email=email,
+            Phone=phone,
+            created_at= datetime.now()
+        )
+        # add and commit individually to get IDs back
+        session.add(user)
+        users.append(user)
+    session.commit()
 
+    start_places = ["NewYork", "NewJersey", "California", "niagarafalls US"]
+    destination_places = ["Chicago", "Ohio", "Connecticut","Nevada"]    
+    
+    trips=[]
+    for i in range (10):
+        trip= Trip(
+            start_place=random.choice(start_places),
+            destination_place=random.choice(destination_places),
+            gas_cost=random.randint(10,555),
+            miles_per_gallon=random.randint(20,65),
+            user=random.choice(users)
+        )
+        session.add(trip)
+        session.commit()
+        trips.append(trip)
+    categories = ["Food","Accomodation","entrance","parking"]
+    expenses=[]
+    for i in range (10):
+        expense= Expense(
+            cost_category=random.choice(categories),
+            amount=random.randint(100,500),
+            trip=random.choice(trips)
+        )
+        session.add(expense)
+        session.commit()
+        expenses.append(expense)
+    
+    #test
+    user_id_to_filter = 1
+    user_trips = session.query(Trip).filter_by(user_id=user_id_to_filter).all()
+    for trip in user_trips:
+        print(trip)
+        # Access associated expenses and print their details
+        for expense in trip.expenses:
+            print("Expense:", expense)
+
+
+    
+    #import ipdb; ipdb.set_trace()
     session.close()
+
+# delete_records()
+# create_user()
+# create_trips()
+# create_expenses()
